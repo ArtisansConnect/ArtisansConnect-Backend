2b125b07bff00dfb54213a50ef009713532f22fb
WailBenoulha <wailbnlh@gmail.com>
2025-04-25 13:37:37 -1100

    build Hvac service api

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -1,6 +1,6 @@
 # serializers.py
 from rest_framework import serializers
-from .models import ElectricalService,PaintingService,FlooringService
+from .models import ElectricalService,PaintingService,FlooringService,HvacService
 
 class ElectricalServiceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,4 +19,10 @@ class FlooringServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = FlooringService
         fields = '__all__'
-        read_only_fields = ['cost']                        
+        read_only_fields = ['cost']   
+
+class HvacServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HvacService
+        fields = ['id', 'user', 'smallHvac', 'mediumHvac', 'bigHvac', 'cost']
+        read_only_fields = ['cost']  
