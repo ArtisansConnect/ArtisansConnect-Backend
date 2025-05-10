1b6c0a63f9ce1518b318564b906cbf87fac8a366
WailBenoulha <wailbnlh@gmail.com>
2025-05-10 06:05:44 -1100

    update serializer for project api

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -64,9 +64,71 @@ class FacadeServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = FacadeService
         fields = '__all__'
-        read_only_fields = ['user','time','cost']                            
+        read_only_fields = ['user','time','cost']     
+
+
+
+
+# Project Serializers  
+                             
+class ElectricalProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ElectricalService
+        fields = ['id','time','cost']
+
+
+class PaintingProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaintingService
+        fields = ['id','time','cost']
+
+class HvacProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HvacService
+        fields = ['id','time','cost']        
+
+class FlooringProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FlooringService
+        fields = ['id','user','cost']         
+
+class PlumbingProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaintingService
+        fields = ['id','time','cost']
+
+class CarpentaryProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WindowsDoorsService
+        fields = ['id','time','cost']
+
+class RoofingProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoofingService
+        fields = ['id','time','cost']
+
+class ConstructionProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConstructionHouseService
+        fields = ['id','time','cost']        
+
+class FacadeProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FacadeService
+        fields = ['id','time','cost']           
+
 
 class ProjectSerializer(serializers.ModelSerializer):
+    electrical = ElectricalProjectSerializer(read_only=True)
+    painting = PaintingProjectSerializer(read_only=True)
+    hvac = HvacProjectSerializer(read_only=True)
+    flooring = FlooringProjectSerializer(read_only=True)
+    plumbing = PlumbingProjectSerializer(read_only=True)
+    carpentary = CarpentaryProjectSerializer(read_only=True)
+    roofing = RoofingProjectSerializer(read_only=True)
+    construction = ConstructionProjectSerializer(read_only=True)
+    facade = FacadeProjectSerializer(read_only=True)
+
     class Meta:
         model = Project
         fields = '__all__'
