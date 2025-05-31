7943b794f1c855144fb45a002e335f26711a68b6
WailBenoulha <wailbnlh@gmail.com>
2025-05-30 14:33:28 -1100

    add blog

--- app/manager/urls.py
+++ app/manager/urls.py
@@ -5,7 +5,8 @@ from .views import (
     RefuseProject,
     ManagerListProject,
     TagView,
-    BlogView
+    BlogView,
+    BlogListView
 )
 
 urlpatterns = [
@@ -15,6 +16,7 @@ urlpatterns = [
     path('planification/<int:pk>/',PlanificationListViewManager.as_view(),name='planification-detail'),  # see only one planification using id
     path('planification/accept/', PlanificationView.as_view(),name='accept=project'),                    # accept project to create a new planification
     path('planification/reject/<int:pk>/', RefuseProject.as_view(),name='refuse-project'),                        # refuse project 
-    path('tags/',TagView.as_view()),
-    path('blogs/create',BlogView.as_view())
+    path('tags/',TagView.as_view(),name='tags-list'),
+    path('blog/',BlogListView.as_view(),name='blogs-list'),
+    path('blogs/create',BlogView.as_view(),name='blog')
 ]
