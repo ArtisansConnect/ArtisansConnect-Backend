44eca062c9b089f335a7beec281f074ce98f600e
WailBenoulha <wailbnlh@gmail.com>
2025-06-03 03:57:19 -1100

    build artisan apis

--- app/artisan/urls.py
+++ app/artisan/urls.py
@@ -3,8 +3,54 @@ from .views import (
     RequestRecrutement,
     UpdateArtisanProfile
 )
+from .views import (
+    ListElectricalTasks, UpdateElectricalProgress,
+    ListPaintingTasks, UpdatePaintingProgress,
+    ListFlooringTasks, UpdateFlooringProgress,
+    ListHvacTasks, UpdateHvacProgress,
+    ListPlumbingTasks, UpdatePlumbingProgress,
+    ListWindowsDoorsTasks, UpdateWindowsDoorsProgress,
+    ListRoofingTasks, UpdateRoofingProgress,
+    ListConstructionHouseTasks, UpdateConstructionHouseProgress,
+    ListFacadeTasks, UpdateFacadeProgress,
+)
 
 urlpatterns = [
     path('recrutement/request/',RequestRecrutement.as_view(),name='request-recrutement'),
-    path('profile/update/',UpdateArtisanProfile.as_view(),name='update-profile')
+    path('profile/update/',UpdateArtisanProfile.as_view(),name='update-profile'),
+     # Electrical
+    path('tasks/electrical/', ListElectricalTasks.as_view(), name='list-electrical-tasks'),
+    path('tasks/electrical/progress/<int:pk>/', UpdateElectricalProgress.as_view(), name='update-electrical-progress'),
+
+    # Painting
+    path('tasks/painting/', ListPaintingTasks.as_view(), name='list-painting-tasks'),
+    path('tasks/painting/progress/<int:pk>/', UpdatePaintingProgress.as_view(), name='update-painting-progress'),
+
+    # Flooring
+    path('tasks/flooring/', ListFlooringTasks.as_view(), name='list-flooring-tasks'),
+    path('tasks/flooring/progress/<int:pk>/', UpdateFlooringProgress.as_view(), name='update-flooring-progress'),
+
+    # HVAC
+    path('tasks/hvac/', ListHvacTasks.as_view(), name='list-hvac-tasks'),
+    path('tasks/hvac/progress/<int:pk>/', UpdateHvacProgress.as_view(), name='update-hvac-progress'),
+
+    # Plumbing
+    path('tasks/plumbing/', ListPlumbingTasks.as_view(), name='list-plumbing-tasks'),
+    path('tasks/plumbing/progress/<int:pk>/', UpdatePlumbingProgress.as_view(), name='update-plumbing-progress'),
+
+    # Windows & Doors
+    path('tasks/windows-doors/', ListWindowsDoorsTasks.as_view(), name='list-windows-doors-tasks'),
+    path('tasks/windows-doors/progress/<int:pk>/', UpdateWindowsDoorsProgress.as_view(), name='update-windows-doors-progress'),
+
+    # Roofing
+    path('tasks/roofing/', ListRoofingTasks.as_view(), name='list-roofing-tasks'),
+    path('tasks/roofing/progress/<int:pk>/', UpdateRoofingProgress.as_view(), name='update-roofing-progress'),
+
+    # Construction House
+    path('tasks/construction-house/', ListConstructionHouseTasks.as_view(), name='list-construction-house-tasks'),
+    path('tasks/construction-house/progress/<int:pk>/', UpdateConstructionHouseProgress.as_view(), name='update-construction-house-progress'),
+
+    # Facade
+    path('tasks/facade/', ListFacadeTasks.as_view(), name='list-facade-tasks'),
+    path('tasks/facade/progress/<int:pk>/', UpdateFacadeProgress.as_view(), name='update-facade-progress'),
 ]
--- app/artisan/views.py
+++ app/artisan/views.py
@@ -4,8 +4,19 @@ from rest_framework.permissions import AllowAny
 from .serializers import (
     RequestRecrutementSerializer,
     UpdateArtisanProfileSerializer,
-    ElectricalTasksArtisan,
+    ElectricalTasksArtisanSerializer,
     ElectricalUpdateProgressSerializer
+)
+from .serializers import (
+    ElectricalTasksArtisanSerializer, ElectricalUpdateProgressSerializer,
+    PaintingTasksArtisanSerializer, PaintingUpdateProgressSerializer,
+    FlooringTasksArtisanSerializer, FlooringUpdateProgressSerializer,
+    HvacTasksArtisanSerializer, HvacUpdateProgressSerializer,
+    PlumbingTasksArtisanSerializer, PlumbingUpdateProgressSerializer,
+    WindowsDoorsTasksArtisanSerializer, WindowsDoorsUpdateProgressSerializer,
+    RoofingTasksArtisanSerializer, RoofingUpdateProgressSerializer,
+    ConstructionHouseTasksArtisanSerializer, ConstructionHouseUpdateProgressSerializer,
+    FacadeTasksArtisanSerializer, FacadeUpdateProgressSerializer
 )
 from rest_framework.response import Response
 from rest_framework import status
@@ -15,6 +26,11 @@ from core.permissions import (
 from core.models import (
     CustomUser,
     ElectricalService
+)
+from core.models import (
+    ElectricalService, PaintingService, FlooringService, HvacService,
+    PlumbingService, WindowsDoorsService, RoofingService,
+    ConstructionHouseService, FacadeService
 )
 from django.shortcuts import get_object_or_404
 
@@ -47,13 +63,15 @@ class UpdateArtisanProfile(APIView):
     
 
 # Each Artisan can see their tasks after authenticate
+
+#Electrical
 class ListElectricalTasks(APIView):
-    serializer_class =  ElectricalTasksArtisan
+    serializer_class =  ElectricalTasksArtisanSerializer
     permission_classes = [IsArtisan]    
 
     def get(self,request):
         instance = ElectricalService.objects.filter(artisan=request.user)
-        serializer = ElectricalTasksArtisan(instance,many=True)
+        serializer = ElectricalTasksArtisanSerializer(instance,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
     
 class UpdateElectricalProgress(APIView):
@@ -62,7 +80,7 @@ class UpdateElectricalProgress(APIView):
 
     def patch(self,request,pk=None):
         try:
-            instance = ElectricalService.objects.filter(pk=pk,artisan=request.user)
+            instance = ElectricalService.objects.get(pk=pk,artisan=request.user)
         except ElectricalService.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer =  ElectricalUpdateProgressSerializer(instance,data=request.data,partial=True)   
@@ -70,3 +88,203 @@ class UpdateElectricalProgress(APIView):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
         return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+    
+# Painting
+class ListPaintingTasks(APIView):
+    serializer_class = PaintingTasksArtisanSerializer
+    permission_classes = [IsArtisan]
+
+    def get(self, request):
+        instance = PaintingService.objects.filter(artisan=request.user)
+        serializer = PaintingTasksArtisanSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdatePaintingProgress(APIView):
+    serializer_class = PaintingUpdateProgressSerializer
+    permission_classes = [IsArtisan]
+
+    def patch(self, request, pk=None):
+        try:
+            instance = PaintingService.objects.get(pk=pk, artisan=request.user)
+        except PaintingService.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = PaintingUpdateProgressSerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Flooring
+class ListFlooringTasks(APIView):
+    serializer_class = FlooringTasksArtisanSerializer
+    permission_classes = [IsArtisan]
+
+    def get(self, request):
+        instance = FlooringService.objects.filter(artisan=request.user)
+        serializer = FlooringTasksArtisanSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdateFlooringProgress(APIView):
+    serializer_class = FlooringUpdateProgressSerializer
+    permission_classes = [IsArtisan]
+
+    def patch(self, request, pk=None):
+        try:
+            instance = FlooringService.objects.get(pk=pk, artisan=request.user)
+        except FlooringService.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = FlooringUpdateProgressSerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# HVAC
+class ListHvacTasks(APIView):
+    serializer_class = HvacTasksArtisanSerializer
+    permission_classes = [IsArtisan]
+
+    def get(self, request):
+        instance = HvacService.objects.filter(artisan=request.user)
+        serializer = HvacTasksArtisanSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdateHvacProgress(APIView):
+    serializer_class = HvacUpdateProgressSerializer
+    permission_classes = [IsArtisan]
+
+    def patch(self, request, pk=None):
+        try:
+            instance = HvacService.objects.get(pk=pk, artisan=request.user)
+        except HvacService.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = HvacUpdateProgressSerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Plumbing
+class ListPlumbingTasks(APIView):
+    serializer_class = PlumbingTasksArtisanSerializer
+    permission_classes = [IsArtisan]
+
+    def get(self, request):
+        instance = PlumbingService.objects.filter(artisan=request.user)
+        serializer = PlumbingTasksArtisanSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdatePlumbingProgress(APIView):
+    serializer_class = PlumbingUpdateProgressSerializer
+    permission_classes = [IsArtisan]
+
+    def patch(self, request, pk=None):
+        try:
+            instance = PlumbingService.objects.get(pk=pk, artisan=request.user)
+        except PlumbingService.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = PlumbingUpdateProgressSerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Windows & Doors
+class ListWindowsDoorsTasks(APIView):
+    serializer_class = WindowsDoorsTasksArtisanSerializer
+    permission_classes = [IsArtisan]
+
+    def get(self, request):
+        instance = WindowsDoorsService.objects.filter(artisan=request.user)
+        serializer = WindowsDoorsTasksArtisanSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdateWindowsDoorsProgress(APIView):
+    serializer_class = WindowsDoorsUpdateProgressSerializer
+    permission_classes = [IsArtisan]
+
+    def patch(self, request, pk=None):
+        try:
+            instance = WindowsDoorsService.objects.get(pk=pk, artisan=request.user)
+        except WindowsDoorsService.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = WindowsDoorsUpdateProgressSerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Roofing
+class ListRoofingTasks(APIView):
+    serializer_class = RoofingTasksArtisanSerializer
+    permission_classes = [IsArtisan]
+
+    def get(self, request):
+        instance = RoofingService.objects.filter(artisan=request.user)
+        serializer = RoofingTasksArtisanSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdateRoofingProgress(APIView):
+    serializer_class = RoofingUpdateProgressSerializer
+    permission_classes = [IsArtisan]
+
+    def patch(self, request, pk=None):
+        try:
+            instance = RoofingService.objects.get(pk=pk, artisan=request.user)
+        except RoofingService.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = RoofingUpdateProgressSerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Construction House
+class ListConstructionHouseTasks(APIView):
+    serializer_class = ConstructionHouseTasksArtisanSerializer
+    permission_classes = [IsArtisan]
+
+    def get(self, request):
+        instance = ConstructionHouseService.objects.filter(artisan=request.user)
+        serializer = ConstructionHouseTasksArtisanSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdateConstructionHouseProgress(APIView):
+    serializer_class = ConstructionHouseUpdateProgressSerializer
+    permission_classes = [IsArtisan]
+
+    def patch(self, request, pk=None):
+        try:
+            instance = ConstructionHouseService.objects.get(pk=pk, artisan=request.user)
+        except ConstructionHouseService.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ConstructionHouseUpdateProgressSerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Facade
+class ListFacadeTasks(APIView):
+    serializer_class = FacadeTasksArtisanSerializer
+    permission_classes = [IsArtisan]
+
+    def get(self, request):
+        instance = FacadeService.objects.filter(artisan=request.user)
+        serializer = FacadeTasksArtisanSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdateFacadeProgress(APIView):
+    serializer_class = FacadeUpdateProgressSerializer
+    permission_classes = [IsArtisan]
+
+    def patch(self, request, pk=None):
+        try:
+            instance = FacadeService.objects.get(pk=pk, artisan=request.user)
+        except FacadeService.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = FacadeUpdateProgressSerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
